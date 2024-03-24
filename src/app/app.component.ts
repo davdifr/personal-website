b2b757395098862121dda86ac5b0d9ac99e0d04d
Davide Di Francesco <davdifr@gmail.com>
2024-03-24 12:59:15 +0100

    Add environment.ts file and set app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'personal-website';
+  #titleService = inject(Title);
+  #appTitle = environment.appTitle;
+
+  constructor() {
+    this.#titleService.setTitle(this.#appTitle);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  appTitle: 'Davide Di Francesco - Portfolio',
+};
