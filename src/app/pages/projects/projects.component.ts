32db9a20542874ace8026e80a7811b6a42ccd2e6
Davide Di Francesco <davdifr@gmail.com>
2024-03-24 19:13:48 +0100

    Add loading spinner and repository cards in projects component

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,22 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Repository } from '../../models/github.models';
 import { HttpClient } from '@angular/common/http';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../models/state.models';
 import { fetchRepositories } from '../../store/repositories/repositories.actions';
+import {
+  selectRepositories,
+  selectRepositoriesLoading,
+} from '../../store/repositories/repositories.selectors';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'projects',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
-export default class ProjectsComponent {
+export default class ProjectsComponent implements OnInit {
   #http = inject(HttpClient);
   #store = inject(Store<AppState>);
 
-  constructor() {
+  repositories$ = this.#store.select(selectRepositories);
+  repositoriesLoading$ = this.#store.select(selectRepositoriesLoading);
+
+  ngOnInit() {
+    this.repositories$.subscribe((repositories) => {
+      // Fetch repositories if there are none
+      if (!repositories.length) {
+        this.fetchRepositories();
+      }
+    });
+  }
+
+  private fetchRepositories() {
     this.#store.dispatch(fetchRepositories());
   }
 }
