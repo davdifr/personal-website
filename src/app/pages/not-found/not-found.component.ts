990006a8bf9e0cf201d6770d11ee5ddc6783f722
Davide Di Francesco <davdifr@gmail.com>
2024-03-25 12:58:52 +0100

    Update styles for not-found and projects components

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
