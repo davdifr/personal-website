318d581e245004d70ed043acc6bf5f743c058d31
Davide Di Francesco <davdifr@gmail.com>
2024-03-26 20:00:13 +0100

    Update header component, theme toggle, and CSS styles

--- src/app/template/header/theme-toggle/theme-toggle.component.ts
+++ src/app/template/header/theme-toggle/theme-toggle.component.ts
@@ -1,17 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostBinding, inject } from '@angular/core';
 import { ThemeService } from '../../../services/theme.service';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'theme-toggle',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './theme-toggle.component.html',
   styleUrl: './theme-toggle.component.css',
 })
 export class ThemeToggleComponent {
   #themeService = inject(ThemeService);
+  rotate = false;
 
   switchTheme(): void {
+    this.rotate = !this.rotate;
     this.#themeService.toggleTheme();
   }
 
